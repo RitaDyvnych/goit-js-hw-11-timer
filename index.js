f69cd52c77b46f6172d64b3132ea8b79f3f9f146
RitaDyvnych <m.dyvnych@gmail.com>
2021-08-16 16:04:23 +0300

    разделено на разные методы вычисления времени и отрисовка интерфейса

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 class CountdownTimer {
-    constructor({ selector, targetDate }) {
+    constructor({ selector, targetDate, onTick }) {
         this.targetDate = targetDate;
         this.id = null;
+        this.onTick = onTick;
         this.refs = {
             days: document.querySelector('[data-value="days"]'),
             hours: document.querySelector('[data-value="hours"]'),
@@ -14,40 +15,44 @@ class CountdownTimer {
         
     init() {
         this.id = setInterval(this.calcTime.bind(this), 1000);
-        
     }
 
     calcTime() {
         const currentDate = Date.now();
-        const time = this.targetDate.getTime() - currentDate;
+        const deltaTime = this.targetDate.getTime() - currentDate;
         
-        if (time < 0) {
+        if (deltaTime < 0) {
             clearInterval(this.id);
             return;
         }
+        const time = this.getTimeComponents(deltaTime);
+        this.onTick(time);
+    }
 
-
+    getTimeComponents(time) {
         const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
         const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
         const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
         const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
 
-        this.refs.days.textContent = days;
-        this.refs.hours.textContent = hours;
-        this.refs.mins.textContent = mins;
-        this.refs.secs.textContent = secs;
-
-
+        return ({ days, hours, mins, secs });
     }
 
     pad(number) {
         return String(number).padStart(2, "0")
     }
 
- 
 }
 
 const timer = new CountdownTimer ({
   selector: '#timer-1',
   targetDate: new Date('Aug 17, 2021'),
+  onTick: updateTime
 });
+
+function updateTime({ days, hours, mins, secs }) {
+        this.refs.days.textContent = days;
+        this.refs.hours.textContent = hours;
+        this.refs.mins.textContent = mins;
+        this.refs.secs.textContent = secs;
+    }
